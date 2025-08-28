6d0eadd57c1f24767a8a9143f72179f676149625
3sL <30650941+3sL@users.noreply.github.com>
2025-08-28 15:05:50 +0300

    login.php

--- login.php
+++ login.php
@@ -1,4 +1,19 @@
 <?php
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "تم استقبال طلب POST بنجاح!";
+} else {
+    echo "الرجاء إرسال البيانات بطريقة POST";
+}
+?>
+
+
+<form action="login.php" method="POST">
+  <input type="email" name="email" required>
+  <input type="password" name="password" required>
+  <button type="submit">تسجيل الدخول</button>
+</form>
+
 session_start();
 include('db.php');  // استدعاء ملف الاتصال بقاعدة البيانات
 
@@ -47,3 +62,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 </body>
 </html>
+
